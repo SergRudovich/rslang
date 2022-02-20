927449b3b6b1a24e4f9494c1d0fc68bfc6c9d928
avshir <av.shirinskaya@gmail.com>
2022-02-20 17:05:52 +0300

    feat: add component gameCard into TextBook

--- src/components/Textbook/Textbook.js
+++ src/components/Textbook/Textbook.js
@@ -6,6 +6,9 @@ import Paginate from './Paginate/Paginate';
 import { useSelector } from "react-redux";
 import { DIFFICULT_CATEGORY } from '../../data/const';
 import { Link } from "react-router-dom";
+import GameCard from './GameCard/GameCard';
+import GameCardImage2 from "./../../assets/best/audio-dog.jpg";
+import GameCardImage1 from "./../../assets/best/sprint-dog-white.jpg";
 
 function Textbook() {
 
@@ -13,23 +16,41 @@ function Textbook() {
   const user = useSelector(state => state.user);
 
   return (
-    <div className='textbook-wrapper'>
-      <h1><Link to="/textbook">Учебник</Link>
-        {user &&
+    <div className="textbook-wrapper">
+      <h1>
+        <Link to="/textbook">Учебник</Link>
+        {user && (
           <>
             <span> | </span>
             <Link to="/learned">Изученные слова </Link>
           </>
-        }
+        )}
       </h1>
       <h3>Уровни сложности слов</h3>
       <WordCategories />
       <h1>Слова</h1>
       <Words />
-      {wordsCategory !== DIFFICULT_CATEGORY &&
-        <Paginate />}
+      {wordsCategory !== DIFFICULT_CATEGORY && <Paginate />}
       <h1>Игры</h1>
       <h3>Закрепи изученное играючи</h3>
+      <div className="game-card__container">
+        <Link to="/sprint">
+          <GameCard
+            gameName="Спринт"
+            gameTarget="Перевод на скорость"
+            gameDescription="Определи как можно быстрее: верный перевод слова или нет"
+            gameImage={GameCardImage1}
+          ></GameCard>
+        </Link>
+        <Link to="/audiocall">
+          <GameCard
+            gameName="Аудиовызов"
+            gameTarget="Аудирование"
+            gameDescription="Определи, какое слово было произнесено"
+            gameImage={GameCardImage2}
+          ></GameCard>
+        </Link>
+      </div>
     </div>
   );
 }
