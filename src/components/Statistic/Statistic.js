c89e2e0a418db54a956b9d56f65d3187e7d7a13d
avshir <av.shirinskaya@gmail.com>
2022-02-20 17:13:48 +0300

    feat: App,  MainPage, Statistic

--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Statistic() {
   return (
     <div className="main__screen bg-color-6">
-      <h1>Statistic</h1>
+      <div className="container">
+        <h1 className="title-2">Статистика</h1>
+      </div>
     </div>
   );
 }
